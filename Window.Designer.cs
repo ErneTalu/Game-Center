149c5fab9fa8deda153d63181d7c8b2a4b6d0c6e
Erne <63594525+ErneTalu@users.noreply.github.com>
2025-02-09 17:07:25 +0300

    Init

---
+++ Window.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Game_Center
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GamesPathTb = new System.Windows.Forms.TextBox();
+            this.SelectFolderB = new System.Windows.Forms.Button();
+            this.SetFolderB = new System.Windows.Forms.Button();
+            this.AddGameB = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.panelGames = new System.Windows.Forms.FlowLayoutPanel();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.SearchTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.Controls.Add(this.GamesPathTb);
+            this.panel1.Controls.Add(this.SelectFolderB);
+            this.panel1.Controls.Add(this.SetFolderB);
+            this.panel1.Controls.Add(this.AddGameB);
+            this.panel1.Controls.Add(this.Title);
+            this.panel1.Controls.Add(this.Logo);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1094, 136);
+            this.panel1.TabIndex = 2;
+            // 
+            // GamesPathTb
+            // 
+            this.GamesPathTb.Location = new System.Drawing.Point(943, 105);
+            this.GamesPathTb.Name = "GamesPathTb";
+            this.GamesPathTb.Size = new System.Drawing.Size(105, 20);
+            this.GamesPathTb.TabIndex = 6;
+            this.GamesPathTb.TextChanged += new System.EventHandler(this.GamesPathTb_TextChanged);
+            // 
+            // SelectFolderB
+            // 
+            this.SelectFolderB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.SelectFolderB.Location = new System.Drawing.Point(936, 3);
+            this.SelectFolderB.Name = "SelectFolderB";
+            this.SelectFolderB.Size = new System.Drawing.Size(120, 28);
+            this.SelectFolderB.TabIndex = 5;
+            this.SelectFolderB.Text = "Select Folder";
+            this.SelectFolderB.UseVisualStyleBackColor = false;
+            this.SelectFolderB.Click += new System.EventHandler(this.SelectFolderB_Click);
+            // 
+            // SetFolderB
+            // 
+            this.SetFolderB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.SetFolderB.Location = new System.Drawing.Point(936, 32);
+            this.SetFolderB.Name = "SetFolderB";
+            this.SetFolderB.Size = new System.Drawing.Size(120, 64);
+            this.SetFolderB.TabIndex = 4;
+            this.SetFolderB.Text = "Set Folder";
+            this.SetFolderB.UseVisualStyleBackColor = false;
+            this.SetFolderB.Click += new System.EventHandler(this.SetFolderB_Click);
+            // 
+            // AddGameB
+            // 
+            this.AddGameB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.AddGameB.Location = new System.Drawing.Point(780, 32);
+            this.AddGameB.Name = "AddGameB";
+            this.AddGameB.Size = new System.Drawing.Size(120, 64);
+            this.AddGameB.TabIndex = 3;
+            this.AddGameB.Text = "Add Game";
+            this.AddGameB.UseVisualStyleBackColor = false;
+            this.AddGameB.Click += new System.EventHandler(this.AddGameB_Click);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.Title.Font = new System.Drawing.Font("Sitka Small", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Title.ForeColor = System.Drawing.SystemColors.Control;
+            this.Title.Location = new System.Drawing.Point(148, 25);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(343, 71);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "Game Center";
+            this.Title.Click += new System.EventHandler(this.Title_Click);
+            // 
+            // Logo
+            // 
+            this.Logo.Image = global::Game_Center.Properties.Resources.ControllerLogo;
+            this.Logo.Location = new System.Drawing.Point(17, 5);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(125, 125);
+            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Logo.TabIndex = 0;
+            this.Logo.TabStop = false;
+            this.Logo.Click += new System.EventHandler(this.Logo_Click);
+            // 
+            // panelGames
+            // 
+            this.panelGames.AutoScroll = true;
+            this.panelGames.Location = new System.Drawing.Point(-2, 193);
+            this.panelGames.Margin = new System.Windows.Forms.Padding(0);
+            this.panelGames.Name = "panelGames";
+            this.panelGames.Size = new System.Drawing.Size(1072, 496);
+            this.panelGames.TabIndex = 3;
+            this.panelGames.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGames_Paint);
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.SearchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SearchBox.Font = new System.Drawing.Font("Malgun Gothic", 12.2F);
+            this.SearchBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.SearchBox.Location = new System.Drawing.Point(24, 151);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(1025, 29);
+            this.SearchBox.TabIndex = 4;
+            this.SearchBox.TextChanged += new System.EventHandler(this.SearchBox_TextChanged);
+            // 
+            // SearchTimer
+            // 
+            this.SearchTimer.Tick += new System.EventHandler(this.SearchTimer_Tick);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1091, 698);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.panelGames);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Window";
+            this.Text = "Game Center";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox Logo;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button AddGameB;
+        private System.Windows.Forms.Button SetFolderB;
+        private System.Windows.Forms.FlowLayoutPanel panelGames;
+        private System.Windows.Forms.Button SelectFolderB;
+        private System.Windows.Forms.TextBox GamesPathTb;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Timer SearchTimer;
+    }
+}
+
---
+++ Window.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Game_Center
+{
+    public partial class Window : Form
+    {
+        string gamesPath;
+        Timer searchTimer;
+
+        public Window()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            searchTimer = new Timer();
+            searchTimer.Interval = 200; 
+            searchTimer.Tick += SearchTimer_Tick; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadPaths();
+        }
+
+        private void Logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Title_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelGames_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void AddGameB_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Executable and URL Files (*.exe;*.url)|*.exe;*.url";
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string selectedExePath in openFileDialog.FileNames)
+                    {
+                        SavePaths(selectedExePath);
+                        CreateGameItem(selectedExePath);
+                    }
+                }
+            }
+        }
+
+        private void SetFolderB_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(gamesPath) || !Directory.Exists(gamesPath))
+            {
+                MessageBox.Show("Please select a games folder first", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<string> selectedFolders = SelectFoldersWithOpenFileDialog();
+            if (selectedFolders.Count > 0)
+            {
+                foreach (string folderPath in selectedFolders)
+                {
+                    MoveFolderToGamePath(folderPath);
+                }
+            }
+        }
+
+        private void SelectFolderB_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                gamesPath = dialog.FileName;
+                GamesPathTb.Text = gamesPath;
+                SavePaths();
+            }
+        }
+
+        private void GamesPathTb_TextChanged(object sender, EventArgs e)
+        {
+            gamesPath = GamesPathTb.Text;
+            SavePaths();
+        }
+
+        private void SearchBox_TextChanged(object sender, EventArgs e)
+        {
+            searchTimer.Stop(); 
+            searchTimer.Start();
+        }
+
+        private void SearchTimer_Tick(object sender, EventArgs e)
+        {
+            searchTimer.Stop();
+
+            string searchText = SearchBox.Text.Trim().ToLower();
+
+            panelGames.SuspendLayout();
+
+            foreach (Control control in panelGames.Controls)
+            {
+                if (control is Panel containerPanel)
+                {
+                    TextBox gameLabel = containerPanel.Controls.OfType<TextBox>().FirstOrDefault();
+
+                    if (gameLabel != null)
+                    {
+                        string gameName = gameLabel.Text.ToLower();
+                        containerPanel.Visible = gameName.Contains(searchText);
+                    }
+                }
+            }
+
+            panelGames.ResumeLayout(); 
+        }
+    
+
+        private void CreateGameItem(string exePath, string gameName = null)
+        {
+            Button newButton = new Button();
+            TextBox newTextBox = new TextBox();
+            Button deleteButton = new Button();
+
+            // Button
+            Icon exeIcon = Icon.ExtractAssociatedIcon(exePath);
+            newButton.BackColor = Color.Transparent;
+            newButton.BackgroundImage = exeIcon.ToBitmap();
+            newButton.BackgroundImageLayout = ImageLayout.Stretch;
+            newButton.FlatStyle = FlatStyle.Flat;
+            newButton.FlatAppearance.BorderSize = 0;
+            newButton.Size = new Size(80, 80);
+            newButton.Tag = exePath;
+            newButton.Click += new EventHandler(OpenExe);
+
+            // Name 
+            if (gameName == null) gameName = Path.GetFileNameWithoutExtension(exePath);
+            newTextBox.BackColor = SystemColors.ActiveCaption;
+            newTextBox.BorderStyle = BorderStyle.None;
+            newTextBox.Text = gameName;
+            newTextBox.Font = new Font("Malgun Gothic", 12);
+            newTextBox.AutoSize = true;
+            newTextBox.Size = new Size(100, 100);
+            newTextBox.ForeColor = Color.WhiteSmoke;
+            newTextBox.Tag = exePath;
+            newTextBox.TextChanged += new EventHandler(GameNameBoxUpdate);
+
+            // Delete Button
+            deleteButton.BackColor = Color.Transparent;
+            deleteButton.BackgroundImage = Properties.Resources.trash; 
+            deleteButton.BackgroundImageLayout = ImageLayout.Zoom;
+            deleteButton.FlatStyle = FlatStyle.Flat;
+            deleteButton.FlatAppearance.BorderSize = 0;
+            deleteButton.Size = new Size(18, 18);
+            deleteButton.Tag = exePath; 
+            deleteButton.Click += new EventHandler(RemoveGameItem); 
+
+            // Panel
+            Panel containerPanel = new Panel();
+            containerPanel.Size = new Size(120, 120);
+            containerPanel.BorderStyle = BorderStyle.None;
+
+            // Adding
+            containerPanel.Controls.Add(deleteButton);
+            containerPanel.Controls.Add(newButton);
+            containerPanel.Controls.Add(newTextBox);
+
+            // Positions
+            newButton.Top = 0;
+            newButton.Left = (containerPanel.Width - newButton.Width + 35) / 2;
+
+            newTextBox.Top = newButton.Bottom + 5;
+            newTextBox.Left = (containerPanel.Width - newTextBox.Width + 35) / 2;
+
+            deleteButton.Top = newButton.Bottom - 17;
+            deleteButton.Left = newButton.Right - 17;
+            panelGames.Controls.Add(containerPanel);
+        }
+        private void RemoveGameItem(object sender, EventArgs e)
+        {
+            Button deleteButton = (Button)sender;
+            string exePath = deleteButton.Tag.ToString();
+
+            foreach (Control control in panelGames.Controls)
+            {
+                if (control is Panel containerPanel)
+                {
+                    foreach (Control innerControl in containerPanel.Controls)
+                    {
+                        if (innerControl is Button button && button.Tag.ToString() == exePath)
+                        {
+                            panelGames.Controls.Remove(containerPanel);
+                            containerPanel.Dispose(); 
+
+                            RemovePath(exePath);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        private void GameNameBoxUpdate(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                string newName = textBox.Text;
+                UpdateGameName((string)textBox.Tag, newName);
+            }
+        }
+
+        private void OpenExe(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string exePath = clickedButton.Tag.ToString();
+            try
+            {
+                Process.Start(exePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open the .exe file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void SavePaths(string exePath = null)
+        {
+            string filePath = "saved.txt";
+            List<string> lines = new List<string>();
+
+            if (File.Exists(filePath))
+            {
+                lines = File.ReadAllLines(filePath).ToList();
+            }
+
+            string gamesPathLine = $"GamesPath: {gamesPath}";
+            if (lines.Count > 0 && lines[0].StartsWith("GamesPath:"))
+            {
+                lines[0] = gamesPathLine; 
+            }
+            else
+            {
+                lines.Insert(0, gamesPathLine); 
+            }
+
+            if (!string.IsNullOrEmpty(exePath) && !lines.Any(line => line.StartsWith(exePath + " |")))
+            {
+                string gameName = Path.GetFileNameWithoutExtension(exePath);
+                lines.Add($"{exePath} | {gameName}");
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
+        private void RemovePath(string exePath)
+        {
+            if (File.Exists("saved.txt"))
+            {
+                string[] exePaths = File.ReadAllLines("saved.txt");
+
+                var updatedExePaths = exePaths.Where(line => !line.StartsWith(exePath + " |")).ToArray();
+
+                File.WriteAllLines("saved.txt", updatedExePaths);
+            }
+        }
+        private void LoadPaths()
+        {
+            string filePath = "saved.txt";
+            List<string> validExePaths = new List<string>();
+
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                if (lines.Length > 0 && lines[0].StartsWith("GamesPath:"))
+                {
+                    gamesPath = lines[0].Replace("GamesPath: ", "");
+                    GamesPathTb.Text = gamesPath;
+                    validExePaths.Add(lines[0]);
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string line = lines[i];
+
+                    string[] parts = line.Split(new string[] { " | " }, StringSplitOptions.None);
+                    if (parts.Length == 2)
+                    {
+                        string exePath = parts[0];
+                        string gameName = parts[1];
+
+                        if (File.Exists(exePath))
+                        {
+                            CreateGameItem(exePath, gameName);
+                            validExePaths.Add(line);
+                        }
+                    }
+                }
+
+                File.WriteAllLines(filePath, validExePaths);
+            }
+        }
+        private void UpdateGameName(string exePath, string newName)
+        {
+            if (File.Exists("saved.txt"))
+            {
+                List<string> lines = File.ReadAllLines("saved.txt").ToList();
+
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    if (lines[i].StartsWith(exePath + " |"))
+                    {
+                        lines[i] = exePath + " | " + newName;
+                        break;
+                    }
+                }
+
+                File.WriteAllLines("saved.txt", lines);
+            }
+        }
+        private void MoveFolderToGamePath(string sourcePath)
+        {
+            string fileName = Path.GetFileName(sourcePath);
+            string destinationPath = Path.Combine(gamesPath, fileName);
+
+            try
+            {
+                if (Directory.Exists(destinationPath))
+                {
+                    Directory.Delete(destinationPath); 
+                }
+
+                Directory.Move(sourcePath, destinationPath); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error :c : {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private List<string> SelectFoldersWithOpenFileDialog()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:";
+            dialog.IsFolderPicker = true;
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                return dialog.FileNames.ToList(); 
+            }
+
+            return new List<string>();
+        }
+    }
+}
